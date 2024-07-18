0470f8c1ca81d83a485e9db4c2eff12e509226c6
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-07-18 19:49:45 +0545

    Song List Item Created

---
+++ lib/widgets/song_list_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+
+class SongListItem extends StatelessWidget {
+  final SongModel song;
+  final AssetsAudioPlayer player;
+  final VoidCallback onTap;
+  const SongListItem({
+    super.key,
+    required this.song,
+    required this.player,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        song.title,
+        style: const TextStyle(color: Colors.white),
+      ),
+      subtitle: Text(
+        song.artist ?? 'Unknown Artist',
+        style: const TextStyle(color: Colors.white70),
+      ),
+      leading: QueryArtworkWidget(
+        id: song.id,
+        type: ArtworkType.AUDIO,
+        nullArtworkWidget: const Icon(Icons.music_note, color: Colors.white),
+      ),
+      // onTap: () async {
+      //   await player.open(
+      //     Audio.file(song.data,
+      //         metas: Metas(
+      //           title: song.title,
+      //           artist: song.artist,
+      //           album: song.album,
+      //         )),
+      //     autoStart: true,
+      //     showNotification: true,
+      //   );
+      // },
+      onTap: onTap,
+    );
+  }
+}
