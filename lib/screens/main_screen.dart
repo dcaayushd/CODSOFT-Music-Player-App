84a804dc91dc43b93643dea73e6cd5af8ce604c1
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-07-20 23:57:34 +0545

    Main Screen Updated

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,13 +1,8 @@
-import 'dart:math' as math;
-
-import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:palette_generator/palette_generator.dart';
-
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:musicify/screens/home_screen.dart';
 import 'package:musicify/screens/library_screen.dart';
-import 'package:musicify/screens/player_screen.dart';
 import 'package:musicify/screens/search_screen.dart';
 import 'package:musicify/utils/utils.dart';
 
@@ -18,24 +13,22 @@ class MainScreen extends StatefulWidget {
   MainScreenState createState() => MainScreenState();
 }
 
-class MainScreenState extends State<MainScreen>
-    with SingleTickerProviderStateMixin {
+class MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
   final player = AssetsAudioPlayer();
-  bool isPlaying = false;
-
-  late final AnimationController _animationController =
-      AnimationController(vsync: this, duration: const Duration(seconds: 3));
+  Color selectedNavColor = Colors.white;
 
   @override
   void initState() {
     super.initState();
-    player.isPlaying.listen((event) {
-      if (mounted) {
-        setState(() {
-          isPlaying = event;
-        });
-      }
+    _updateNavColor();
+  }
+
+  void _updateNavColor() async {
+    // final colors = await SharedLogic.getImageColors(player);
+    final colors = await getImageColors(player);
+    setState(() {
+      selectedNavColor = colors.lightMutedColor?.color ?? Colors.white;
     });
   }
 
@@ -45,123 +38,49 @@ class MainScreenState extends State<MainScreen>
       body: IndexedStack(
         index: _currentIndex,
         children: [
-          HomeScreen(player: player),
-          const SearchScreen(),
-          const LibraryScreen(),
+          const HomeScreen(),
+          SearchScreen(player: player),
+          LibraryScreen(player: player),
         ],
       ),
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          player.getCurrentAudioImage == null
-              ? const SizedBox.shrink()
-              : FutureBuilder<PaletteGenerator>(
-                  future: getImageColors(player),
-                  builder: (context, snapshot) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 10),
-                      height: 75,
-                      decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: const Alignment(0, 5),
-                              colors: [
-                                snapshot.data?.lightMutedColor?.color ??
-                                    Colors.grey,
-                                snapshot.data?.mutedColor?.color ?? Colors.grey,
-                              ]),
-                          borderRadius: BorderRadius.circular(20)),
-                      child: ListTile(
-                        leading: AnimatedBuilder(
-                          animation: _animationController,
-                          builder: (_, child) {
-                            if (!isPlaying) {
-                              _animationController.stop();
-                            } else {
-                              _animationController.forward();
-                              _animationController.repeat();
-                            }
-                            return Transform.rotate(
-                                angle: _animationController.value * 2 * math.pi,
-                                child: child);
-                          },
-                          child: CircleAvatar(
-                              radius: 30,
-                              backgroundColor: Colors.grey,
-                              backgroundImage: AssetImage(
-                                  player.getCurrentAudioImage?.path ?? '')),
-                        ),
-                        onTap: () => Navigator.push(
-                            context,
-                            CupertinoPageRoute(
-                                fullscreenDialog: true,
-                                builder: (context) => PlayerScreen(
-                                      player: player,
-                                    ))),
-                        title: Text(player.getCurrentAudioTitle),
-                        subtitle: Text(player.getCurrentAudioArtist),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () async {
-                                await player.playOrPause();
-                              },
-                              icon: isPlaying
-                                  ? const Icon(CupertinoIcons.pause)
-                                  : const Icon(CupertinoIcons.play_arrow),
-                            ),
-                            IconButton(
-                              onPressed: () async {
-                                await player.next();
-                              },
-                              icon: const Icon(
-                                CupertinoIcons.forward_end,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
+      bottomNavigationBar:
+
+          // FutureBuilder<PaletteGenerator>(
+          //   future: getImageColors(player),
+          //   builder: (context, snapshot) {
+          //     final selectedColor =
+          //         snapshot.data?.lightMutedColor?.color ?? Colors.white;
+          //     return
+
+          Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[850],
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
                 ),
-          FutureBuilder<PaletteGenerator>(
-            future: getImageColors(player),
-            builder: (context, snapshot) {
-              final selectedColor =
-                  snapshot.data?.lightMutedColor?.color ?? Colors.white;
-              return Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[850],
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
+              ),
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 8.0, right: 8.0, bottom: 8, top: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _buildNavItem(CupertinoIcons.square_grid_2x2, 'Home', 0,
+                          selectedNavColor),
+                      _buildNavItem(
+                          CupertinoIcons.search, 'Search', 1, selectedNavColor),
+                      _buildNavItem(
+                          Icons.library_music, 'Library', 2, selectedNavColor),
+                    ],
                   ),
                 ),
-                child: SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 16.0, right: 16.0, bottom: 0, top: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        _buildNavItem(CupertinoIcons.square_grid_2x2, 'Home', 0,
-                            selectedColor),
-                        _buildNavItem(
-                            CupertinoIcons.search, 'Search', 1, selectedColor),
-                        _buildNavItem(CupertinoIcons.music_note_list, 'Library',
-                            2, selectedColor),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
+              )
+              // ,
+              // );
+              //     },
+              ),
     );
   }
 
@@ -186,5 +105,11 @@ class MainScreenState extends State<MainScreen>
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
   }
 }
