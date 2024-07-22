21ec20fed94dc5e7aca42cd6796bdf6b76060193
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-07-22 21:43:35 +0545

    Song List Item Error Fixed

--- lib/widgets/song_list_item.dart
+++ lib/widgets/song_list_item.dart
@@ -34,17 +34,23 @@ class SongListItem extends StatelessWidget {
           color: isSelected ? Colors.blue.withOpacity(0.7) : Colors.white70,
         ),
       ),
-      leading: song.id != null
-          ? QueryArtworkWidget(
-              id: song.id!,
-              type: ArtworkType.AUDIO,
-              nullArtworkWidget: _defaultArtwork(),
-            )
-          : _defaultArtwork(),
+      leading: _buildArtwork(),
       onTap: onTap,
       trailing:
           isSelected ? const Icon(Icons.equalizer, color: Colors.blue) : null,
     );
+  }
+
+  Widget _buildArtwork() {
+    if (song.id != null) {
+      return QueryArtworkWidget(
+        id: song.id!,
+        type: ArtworkType.AUDIO,
+        nullArtworkWidget: _defaultArtwork(),
+      );
+    } else {
+      return _defaultArtwork();
+    }
   }
 
   Widget _defaultArtwork() {
